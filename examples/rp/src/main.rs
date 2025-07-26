d444230e6e4237f165443868a9b3e7906402b0ab
Cedian03 <nyaalgotjohansson@gmail.com>
2025-07-26 18:14:50 +0200

    add logging to example

--- examples/rp/src/main.rs
+++ examples/rp/src/main.rs
@@ -1,17 +1,19 @@
 #![no_std]
 #![no_main]
 
+use panic_probe as _;
+#[cfg(feature = "defmt")]
+use defmt_rtt as _;
+
 use embassy_executor::Spawner;
 use embassy_rp::{
-    gpio::{Input, Level, Output, Pull}, 
-    peripherals::USB, 
+    bind_interrupts,
+    gpio::{Input, Level, Output, Pull},
+    peripherals::USB,
     usb::{Driver, InterruptHandler},
-    bind_interrupts, 
 };
 
 use okey::prelude::*;
-
-use panic_probe as _;
 
 bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => InterruptHandler<USB>;
@@ -20,7 +22,7 @@ bind_interrupts!(struct Irqs {
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    
+
     let driver = Driver::new(p.USB, Irqs);
     let config = Config::default();
     let mut state = State::default();
@@ -31,7 +33,7 @@ async fn main(_spawner: Spawner) {
             Output::new(p.PIN_0, Level::Low),
             Output::new(p.PIN_1, Level::Low),
         ];
-    
+
         let rows = [
             Input::new(p.PIN_10, Pull::Down),
             Input::new(p.PIN_11, Pull::Down),
